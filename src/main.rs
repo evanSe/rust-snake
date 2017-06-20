5bc7aa93efeda307eaf70bb2137dd116ea04103c
Peter Czibik <p.czibik@gmail.com>
2017-06-20 22:28:18 +0200

    add wall teleport functionality

--- src/main.rs
+++ src/main.rs
@@ -42,17 +42,34 @@ struct Food {
 }
 
 impl Snake {
-    fn move_body (&mut self, is_growing: &bool) {
+    fn move_body (&mut self, is_growing: &bool, main_window_x: &i32, main_window_y: &i32) {
         self.body.insert(0, self.head);
         match self.direction {
             Direction::Up => self.head.y -= 1,
             Direction::Down => self.head.y += 1,
             Direction::Right => self.head.x += 1,
             Direction::Left => self.head.x -= 1
-        }
+        };
+
+        if self.head.y < 0 {
+            self.head.y = *main_window_y - 1;
+        };
+
+        if self.head.x < 0 {
+            self.head.x = *main_window_x - 1;
+        };
+
+        if self.head.y >= *main_window_y {
+            self.head.y = 0;
+        };
+
+        if self.head.x >= *main_window_x {
+            self.head.x = 0;
+        };
+
         if !is_growing {
             self.body.pop();
-        }
+        };
     }
 
     fn change_direction (&mut self, new_direction: Direction) {
@@ -105,7 +122,7 @@ fn main() {
     let (y, x) = border_window.get_beg_yx();
     let (lines, columns) = border_window.get_max_yx();
     let main_window = newwin(lines - 2, columns - 2, y + 1, x + 1);
-    let (main_window_y, main_window_x) = border_window.get_max_yx();
+    let (main_window_y, main_window_x) = main_window.get_max_yx();
     main_window.nodelay(true);
     main_window.keypad(true);
     noecho();
@@ -131,7 +148,7 @@ fn main() {
     loop {
         let food_eaten = player.eat(&food);
         draw_field(&main_window, &player, &food);
-        player.move_body(&food_eaten);
+        player.move_body(&food_eaten, &main_window_x, &main_window_y);
         if food_eaten {
             food = Food {
                 position: Point {
@@ -153,4 +170,14 @@ fn main() {
         let sleep_time = Duration::from_millis(100);
         thread::sleep(sleep_time)
     }
+
+    main_window.nodelay(false);
+
+    let message = "winner winner chicken dinner!".to_string();
+    main_window.mv(10, 10);
+    for ref item in message.chars() {
+        main_window.addch(*item);
+    };
+    main_window.refresh();
+    let keypress = main_window.getch();
 }
